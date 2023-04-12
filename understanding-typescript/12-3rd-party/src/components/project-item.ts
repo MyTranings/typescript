a8c7846b316a7a1989d2506ce4454b89f461feb4
dgeorgiev <d.georgiev@astoundcommerce.com>
2023-04-12 16:42:39 +0300

    12 - 3rd party libraries - initial commit

---
+++ understanding-typescript/12-3rd-party/src/components/project-item.ts
@@ -0,0 +1,46 @@
+import Component from "./base-component";
+import { Draggable } from "../modules/drag-drop";
+import { Project } from "../modules/project";
+import { AutoBind } from "../decorators/autobind";
+
+export class ProjectItem
+  extends Component<HTMLUListElement, HTMLLIElement>
+  implements Draggable
+{
+  private project: Project;
+
+  get persons() {
+    return this.project.people === 1
+      ? "1 person"
+      : `${this.project.people} persons`;
+  }
+
+  constructor(hostId: string, project: Project) {
+    super("single-project", hostId, false, project.id);
+    this.project = project;
+
+    this.configure();
+    this.renderContent();
+  }
+
+  renderContent() {
+    this.element.querySelector("h2")!.textContent = this.project.title;
+    this.element.querySelector("h3")!.textContent = this.persons + " assigned";
+    this.element.querySelector("p")!.textContent = this.project.description;
+  }
+
+  configure() {
+    this.element.addEventListener("dragstart", this.dragStartHandler);
+    this.element.addEventListener("dragend", this.dragEndHandler);
+  }
+
+  @AutoBind
+  dragStartHandler(event: DragEvent): void {
+    event.dataTransfer!.setData("text/plain", this.project.id);
+    event.dataTransfer!.effectAllowed = "move";
+  }
+
+  dragEndHandler(_: DragEvent): void {
+    console.log("DragEnd");
+  }
+}
---
+++ understanding-typescript/12-3rd-party/src/decorators/autobind.ts
@@ -0,0 +1,13 @@
+export function AutoBind(_: any, _1: string, descriptor: PropertyDescriptor) {
+  const originalMethod = descriptor.value;
+  const adjDescriptor = {
+    configurable: true,
+    get() {
+      const boundFn = originalMethod.bind(this);
+
+      return boundFn;
+    },
+  };
+
+  return adjDescriptor;
+}
