adc2632161aebeb6e8d006f0d1a595fdad459e52
dgeorgiev <d.georgiev@astoundcommerce.com>
2023-04-12 17:39:46 +0300

    12 - Class transformer library

--- understanding-typescript/12-3rd-party/src/app.ts
+++ understanding-typescript/12-3rd-party/src/app.ts
@@ -1,5 +1,23 @@
-import _ from "lodash";
+import "reflect-metadata";
+import { plainToClass } from "class-transformer";
 
-console.log(_.shuffle([1, 2, 3]));
+import { Product } from "./product.model";
 
-console.log(GLOBAL);
+// const p1 = new Product("A book", 12.99);
+
+// console.log(p1.getInfromation());
+
+const products = [
+  { title: "A Carpet", price: 29.99 },
+  { title: "A Book", price: 10.99 },
+];
+
+// const loadedProducts = products.map((prod) => {
+//   return new Product(prod.title, prod.price);
+// });
+
+const loadedProducts = plainToClass(Product, products);
+
+for (const prod of loadedProducts) {
+  console.log(prod.getInfromation());
+}
