b62a4621ed38dd1c3d8d17c1ecb09cbd843f3f90
dgeorgiev <d.georgiev@astoundcommerce.com>
2023-04-12 16:44:29 +0300

    12 - 3rd party libraries - intial commit

--- understanding-typescript/12-3rd-party/src/app.ts
+++
@@ -1,6 +0,0 @@
-import { ProjectInput } from "./components/project-input";
-import { ProjectList } from "./components/project-list";
-
-new ProjectInput("project-input", "app");
-new ProjectList("project-list", "app", "active");
-new ProjectList("project-list", "app", "finished");
