f77f4b30451e80a767f0c442b2073d86ee55e6eb
dgeorgiev <d.georgiev@astoundcommerce.com>
2023-04-18 15:39:42 +0300

    14 - React & TS - add new todo with using hooks and state

---
+++ understanding-typescript/14-react-and-typescript/src/components/NewTodo.tsx
@@ -0,0 +1,28 @@
+import React, { useRef } from "react";
+
+type NewTodoProps = {
+  onAddTodo: (todoText: string) => void;
+};
+
+const NewTodo: React.FC<NewTodoProps> = (props) => {
+  const textInputRef = useRef<HTMLInputElement>(null);
+
+  const todoSumbitHandler = function (event: React.FormEvent) {
+    event.preventDefault();
+    const enteredText = textInputRef.current!.value;
+
+    props.onAddTodo(enteredText);
+  };
+
+  return (
+    <form onSubmit={todoSumbitHandler}>
+      <div>
+        <label htmlFor="todo-text">Todo Text</label>
+        <input type="text" id="todo-text" ref={textInputRef} />
+      </div>
+      <button type="submit">Add To Do</button>
+    </form>
+  );
+};
+
+export default NewTodo;
